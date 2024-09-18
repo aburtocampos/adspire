95f7422dc76a664648c7a934cd2c069e0632628a
Enrique <eabucam@gmail.com>
2024-09-18 17:31:26 -0600

    Interface and Implementation of Repository and HttpResponseWrapper

---
+++ Adspire/Adspire.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,62 @@
+﻿using Adspire.Backend.Data;
+using Adspire.Shared.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adspire.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public CountriesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)
+        {
+            _dataContext.Add(country);
+            await _dataContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _dataContext.Countries.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _dataContext.Countries.FindAsync(id);
+            if (country == null) { return NotFound(); }
+            return Ok(await _dataContext.Countries.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            var currentcountry = await _dataContext.Countries.FindAsync(country.Id);
+            if (currentcountry == null) { return NotFound(); }
+            _dataContext.Update(currentcountry);
+            await _dataContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var currentcountry = await _dataContext.Countries.FindAsync(id);
+            if (currentcountry == null) { return NotFound(); }
+            _dataContext.Remove(currentcountry);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Adspire/Adspire.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+namespace Adspire.Frontend.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+            switch (statusCode)
+            {
+                case HttpStatusCode.NotFound:
+                    return "Recurso no encontrado";
+
+                case HttpStatusCode.BadRequest:
+                    return await HttpResponseMessage.Content.ReadAsStringAsync();
+
+                case HttpStatusCode.Unauthorized:
+                    return "Tienes que estar logueado para ejecutar esta operación";
+
+                case HttpStatusCode.Forbidden:
+                    return "No tienes permiso para ejecutar esta operación";
+
+                default:
+                    return "Ha ocurrido un error inesperado";
+            }
+        }
+    }
+}
